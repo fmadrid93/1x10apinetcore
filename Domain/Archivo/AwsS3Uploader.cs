b0399d53d08a4299dc703e60258ae1b1f24cc4a2
fmadrid93 <31596122+fmadrid93@users.noreply.github.com>
2026-08-18 11:15:03 -0400

    Remove hardcoded AWS credentials in AwsS3Uploader
    Removed hardcoded AWS credentials for security reasons.

--- Domain/Archivo/AwsS3Uploader.cs
+++ Domain/Archivo/AwsS3Uploader.cs
@@ -6,9 +6,9 @@ namespace Domain.Archivo
     public class AwsS3Uploader : S3UploaderBase
     {
         public AwsS3Uploader() : base(
-            bucketName: "datatrends012025",
-            accessKey: "AKIA6JR6HAJHM4XQGYWZ",//Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"),
-            secretKey: "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",//Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"),
+            bucketName: "",
+            accessKey: "",//Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"),
+            secretKey: "",//Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"),
             config: new AmazonS3Config
             {
                 RegionEndpoint = RegionEndpoint.USEast1
